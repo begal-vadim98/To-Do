c3ba4d44a866d554c46569ade3bbb8495ede6b97
Begal Wadim <begal-vadim98@yandex.ru>
2022-02-14 12:58:48 +0300

    изменения файла

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const functionTodo = function() {
+  const todoControl = document.querySelector('.todo-control'),
+    headerInput = document.querySelector('.header-input'),
+    todoList = document.querySelector('.todo-list'),
+    todoCompleted = document.querySelector('.todo-completed');
+    console.log(todoControl)
+    console.log(headerInput)
+    console.log(todoList)
+    console.log(todoCompleted)
+}
+functionTodo();
